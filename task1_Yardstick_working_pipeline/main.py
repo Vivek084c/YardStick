c92e03e288fc1761888eb854ce17e78a194df325
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2025-01-20 21:06:44 +0530

    update

--- task1_Yardstick_working_pipeline/main.py
+++ task1_Yardstick_working_pipeline/main.py
@@ -1,17 +1,19 @@
 from pipelien.download_and_transcript import Stage_download_video, Stage_Transcribe,Stage_generate_audio
 
 
-# STAGE_NAME  = "Download_YT_Video"
-# p = Stage_download_video()
-# filename = p.main()
+STAGE_NAME  = "Download_YT_Video"
+p = Stage_download_video()
+filename = p.main()
 
 
 STAGE_NAME = "Transcribe_YT_Video"
 p = Stage_Transcribe(filename="raw_1")
 filename = p.main()
 
-# STAGE_NAME = "Generate _audio_from_text"
-# p = Stage_generate_audio(filename)
-# p.main()
+STAGE_NAME = "Generate _audio_from_text"
+p = Stage_generate_audio(filename)
+p.main()
+
+
 
 
